1ceee09ef476c91e0b7c2a44130c3687d313b1e9
Steve Block <steveblock@google.com>
2011-04-21 00:36:30 +0100

    Fix build for host executable 'mesa'
    Change-Id: Ie78b546a95995b2bea3ca49abadbfe614a5fcfc1

--- test/Android.mk
+++ test/Android.mk
@@ -30,6 +30,7 @@ LOCAL_SRC_FILES := $(mesa_SRC_FILES)
 LOCAL_STATIC_LIBRARIES := libMesa
 LOCAL_SHARED_LIBRARIES := libbcc
 LOCAL_C_INCLUDES := $(mesa_C_INCLUDES)
+LOCAL_LDLIBS := -lpthread -ldl
 
 include $(LLVM_ROOT_PATH)/llvm-host-build.mk
 include $(BUILD_HOST_EXECUTABLE)
