ed1d0b73eee2c497b0ede061a1f19874c7f9b2da
Chih-Wei Huang <cwhuang@linux.org.tw>
2015-01-24 03:53:48 +0800

    android: a workaround for GL_INVALID_OPERATION (0x502) error
    Inspired by the nice work of Paulo Sergio (pstglia):
    
    "Bypass a check for external target enabled only for images
    created with EGL_EXT_image_dma_buf_import - this makes
    Android graphical components receive a GL_INVALID_OPERATION (0x502)."

--- src/mesa/drivers/dri/i965/intel_tex_image.c
+++ src/mesa/drivers/dri/i965/intel_tex_image.c
@@ -333,6 +333,7 @@ intel_image_target_texture_2d(struct gl_context *ctx, GLenum target,
    if (image == NULL)
       return;
 
+#ifndef ANDROID
    /* We support external textures only for EGLImages created with
     * EGL_EXT_image_dma_buf_import. We may lift that restriction in the future.
     */
@@ -342,6 +343,7 @@ intel_image_target_texture_2d(struct gl_context *ctx, GLenum target,
                "for images created with EGL_EXT_image_dma_buf_import");
       return;
    }
+#endif
 
    /* Disallow depth/stencil textures: we don't have a way to pass the
     * separate stencil miptree of a GL_DEPTH_STENCIL texture through.
