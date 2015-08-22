3ecddea2bf41c15dfc4bce0d2b710b7b930348d4
Mauro Rossi <issor.oruam@gmail.com>
2015-08-22 11:07:34 +0200

    android: build with c++11 on android lollipop
    lollipop-x86 needs -std=c++11 (build with libcxx),
    while kitkat-x86 cannot use -std=c++11
    
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

--- Android.common.mk
+++ Android.common.mk
@@ -79,7 +79,7 @@ LOCAL_CFLAGS += \
 endif
 
 LOCAL_CPPFLAGS += \
-	$(if $(filter true,$(MESA_LOLLIPOP_BUILD)),-D_USING_LIBCXX) \
+	$(if $(filter true,$(MESA_LOLLIPOP_BUILD)),-std=c++11) \
 	-Wno-error=non-virtual-dtor \
 	-Wno-non-virtual-dtor
 
