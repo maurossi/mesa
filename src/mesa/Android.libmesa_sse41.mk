2d65e56ddfe7a21e56afc79a6396c9645040f35e
Rob Herring <robh@kernel.org>
2016-02-02 14:45:07 -0600

    Android: fix x86 gallium builds
    Builds with gallium enabled fail on x86 with linker error:
    
    external/mesa3d/src/mesa/vbo/vbo_exec_array.c:127: error: undefined reference to '_mesa_uint_array_min_max'
    
    The problem is sse_minmax.c is not included in the libmesa_st_mesa
    library. Since the SSE4.1 files are needed for both libmesa_st_mesa
    and libmesa_dricore, move SSE4.1 files into a separate static library
    that can be used by both.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- src/mesa/Android.libmesa_sse41.mk
+++ src/mesa/Android.libmesa_sse41.mk
@@ -20,52 +20,25 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-# ----------------------------------------------------------------------
-# libmesa_st_mesa.a
-# ----------------------------------------------------------------------
-
-ifeq ($(strip $(MESA_BUILD_GALLIUM)),true)
+ifeq ($(ARCH_X86_HAVE_SSE4_1),true)
 
 LOCAL_PATH := $(call my-dir)
 
-# Import variables:
-# 	MESA_GALLIUM_FILES.
-# 	X86_FILES
 include $(LOCAL_PATH)/Makefile.sources
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libmesa_st_mesa
+LOCAL_MODULE := libmesa_sse41
 
-LOCAL_SRC_FILES := \
-	$(MESA_GALLIUM_FILES)
-
-ifeq ($(strip $(MESA_ENABLE_ASM)),true)
-ifeq ($(TARGET_ARCH),x86)
-	LOCAL_SRC_FILES += $(X86_FILES)
-endif # x86
-endif # MESA_ENABLE_ASM
-
-ifeq ($(ARCH_X86_HAVE_SSE4_1),true)
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-	libmesa_sse41
-LOCAL_CFLAGS := \
-       -DUSE_SSE41
-endif
+LOCAL_SRC_FILES += \
+	$(X86_SSE41_FILES)
 
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/mapi \
-	$(MESA_TOP)/src/mesa/main \
-	$(MESA_TOP)/src/glsl \
-	$(MESA_TOP)/src/compiler/nir \
-	$(MESA_TOP)/src/gallium/auxiliary \
-	$(MESA_TOP)/src/gallium/include
+	$(MESA_TOP)/src/gallium/include \
+	$(MESA_TOP)/src/gallium/auxiliary
 
-LOCAL_WHOLE_STATIC_LIBRARIES += \
-	libmesa_program
-
-include $(LOCAL_PATH)/Android.gen.mk
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
-endif # MESA_BUILD_GALLIUM
+endif
