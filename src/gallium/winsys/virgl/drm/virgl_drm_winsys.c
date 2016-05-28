2ab215792296da6d4f69e5dede27cf0cab6ddcea
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-05-28 12:04:54 +0800

    virgl: fix null pointer exceptions

--- src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
+++ src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
@@ -563,6 +563,7 @@ static void virgl_drm_resource_wait(struct virgl_winsys *qws,
 static boolean virgl_drm_lookup_res(struct virgl_drm_cmd_buf *cbuf,
                                     struct virgl_hw_res *res)
 {
+   if (!res) return false;
    unsigned hash = res->res_handle & (sizeof(cbuf->is_handle_added)-1);
    int i;
 
@@ -585,6 +586,7 @@ static void virgl_drm_add_res(struct virgl_drm_winsys *qdws,
                               struct virgl_drm_cmd_buf *cbuf,
                               struct virgl_hw_res *res)
 {
+   if (!res) return;
    unsigned hash = res->res_handle & (sizeof(cbuf->is_handle_added)-1);
 
    if (cbuf->cres >= cbuf->nres) {
