dd6df9012954ee1914a8c3ae2775a6eac3cf0b50
Mauro Rossi <issor.oruam@gmail.com>
2016-06-04 19:26:26 -0400

    WIP nouveau: add locking (v4)
    (v1) Initial version
    
    (v2) Modified due to fd33a6b ("gallium: Use C11 thread abstractions.")
    
         replace pipe_mutex with mtx_t
         replace pipe_mutex_init() with mtx_init()
         replace pipe_mutex_destroy() with mtx_destroy()
         replace pipe_mutex_lock() with mtx_lock()
         replace pipe_mutex_unlock() with mtx_unlock()
    
    (v3) Modified due to c12f830 ("nv50,nvc0: remove IDX from bufctx immediately, to avoid conflicts with clear")
    
    (v4) Modified due to 8eb12147 ("nvc0: add support for bindless textures on kepler+")

--- src/gallium/drivers/nouveau/nvc0/nvc0_query_hw.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_query_hw.c
@@ -154,6 +154,7 @@ nvc0_hw_begin_query(struct nvc0_context *nvc0, struct nvc0_query *q)
    }
    hq->sequence++;
 
+   mtx_lock(&nvc0->screen->base.push_mutex);
    switch (q->type) {
    case PIPE_QUERY_OCCLUSION_COUNTER:
    case PIPE_QUERY_OCCLUSION_PREDICATE:
@@ -199,6 +200,7 @@ nvc0_hw_begin_query(struct nvc0_context *nvc0, struct nvc0_query *q)
    default:
       break;
    }
+   mtx_unlock(&nvc0->screen->base.push_mutex);
    hq->state = NVC0_HW_QUERY_STATE_ACTIVE;
    return ret;
 }
@@ -222,6 +224,7 @@ nvc0_hw_end_query(struct nvc0_context *nvc0, struct nvc0_query *q)
    }
    hq->state = NVC0_HW_QUERY_STATE_ENDED;
 
+   mtx_lock(&nvc0->screen->base.push_mutex);
    switch (q->type) {
    case PIPE_QUERY_OCCLUSION_COUNTER:
    case PIPE_QUERY_OCCLUSION_PREDICATE:
@@ -278,6 +281,7 @@ nvc0_hw_end_query(struct nvc0_context *nvc0, struct nvc0_query *q)
    default:
       break;
    }
+   mtx_unlock(&nvc0->screen->base.push_mutex);
    if (hq->is64bit)
       nouveau_fence_ref(nvc0->screen->base.fence.current, &hq->fence);
 }
@@ -300,17 +304,22 @@ nvc0_hw_get_query_result(struct nvc0_context *nvc0, struct nvc0_query *q,
       nvc0_hw_query_update(nvc0->screen->base.client, q);
 
    if (hq->state != NVC0_HW_QUERY_STATE_READY) {
+      mtx_lock(&nvc0->screen->base.push_mutex);
       if (!wait) {
          if (hq->state != NVC0_HW_QUERY_STATE_FLUSHED) {
             hq->state = NVC0_HW_QUERY_STATE_FLUSHED;
             /* flush for silly apps that spin on GL_QUERY_RESULT_AVAILABLE */
             PUSH_KICK(nvc0->base.pushbuf);
          }
+         mtx_unlock(&nvc0->screen->base.push_mutex);
          return false;
       }
-      if (nouveau_bo_wait(hq->bo, NOUVEAU_BO_RD, nvc0->screen->base.client))
+      if (nouveau_bo_wait(hq->bo, NOUVEAU_BO_RD, nvc0->screen->base.client)) {
+         mtx_unlock(&nvc0->screen->base.push_mutex);
          return false;
+      }
       NOUVEAU_DRV_STAT(&nvc0->screen->base, query_sync_count, 1);
+      mtx_unlock(&nvc0->screen->base.push_mutex);
    }
    hq->state = NVC0_HW_QUERY_STATE_READY;
 
@@ -378,6 +387,8 @@ nvc0_hw_get_query_result_resource(struct nvc0_context *nvc0,
 
    assert(!hq->funcs || !hq->funcs->get_query_result);
 
+   mtx_lock(&nvc0->screen->base.push_mutex);
+
    if (index == -1) {
       /* TODO: Use a macro to write the availability of the query */
       if (hq->state != NVC0_HW_QUERY_STATE_READY)
@@ -386,6 +397,7 @@ nvc0_hw_get_query_result_resource(struct nvc0_context *nvc0,
       nvc0->base.push_cb(&nvc0->base, buf, offset,
                          result_type >= PIPE_QUERY_TYPE_I64 ? 2 : 1,
                          ready);
+      mtx_unlock(&nvc0->screen->base.push_mutex);
       return;
    }
 
@@ -472,6 +484,8 @@ nvc0_hw_get_query_result_resource(struct nvc0_context *nvc0,
    }
    PUSH_DATAh(push, buf->address + offset);
    PUSH_DATA (push, buf->address + offset);
+
+   mtx_unlock(&nvc0->screen->base.push_mutex);
 
    if (buf->mm) {
       nouveau_fence_ref(nvc0->screen->base.fence.current, &buf->fence);
--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -545,7 +545,9 @@ nvc0_screen_destroy(struct pipe_screen *pscreen)
        * _current_ one, and remove both.
        */
       nouveau_fence_ref(screen->base.fence.current, &current);
+      mtx_lock(&screen->base.push_mutex);
       nouveau_fence_wait(current, NULL);
+      mtx_unlock(&screen->base.push_mutex);
       nouveau_fence_ref(NULL, &current);
       nouveau_fence_ref(NULL, &screen->base.fence.current);
    }
