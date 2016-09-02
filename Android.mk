57b3b3366612f76a9e8bf0f56f114e1ad0307dd7
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-09-02 14:55:57 +0800

    android: disable asm temporarily
    It causes 32-bit libglapi.so has text relocations issue. The linker
    of Android 7.0 refuses to load it so all 32-bit apps can't run.
    Disable it until we come out a proper solution.

--- Android.mk
+++ Android.mk
@@ -83,12 +83,7 @@ endif
 
 $(foreach d, $(MESA_BUILD_CLASSIC) $(MESA_BUILD_GALLIUM), $(eval $(d) := true))
 
-# host and target must be the same arch to generate matypes.h
-ifeq ($(TARGET_ARCH),$(HOST_ARCH))
-MESA_ENABLE_ASM := true
-else
 MESA_ENABLE_ASM := false
-endif
 
 ifneq ($(filter true, $(HAVE_GALLIUM_RADEONSI) $(HAVE_GALLIUM_LLVMPIPE)),)
 MESA_ENABLE_LLVM := true
