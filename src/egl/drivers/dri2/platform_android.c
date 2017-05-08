83a058d1f519ba011ff03b055b0e0849c575880b
Mauro Rossi <issor.oruam@gmail.com>
2017-05-09 00:58:18 +0200

    egl: fold Android logger into main/ (swrast fix) (v2)
    Fixes the folloing building error:
    
    external/mesa/src/egl/drivers/dri2/platform_android.c:1492:19:
    error: use of undeclared identifier 'droid_log'
       _eglSetLogProc(droid_log);
                      ^
    1 error generated.
    
    (v2) renamed dpy to disp after commit aa187fe7bfac856207a5feee36e17120a6ee9d22
    "egl/android: Fix build break with dri2_initialize_android _EGLDisplay parameter"

--- src/egl/drivers/dri2/platform_android.c
+++ src/egl/drivers/dri2/platform_android.c
@@ -1606,8 +1606,6 @@ dri2_initialize_android(_EGLDriver *drv, _EGLDisplay *disp)
 {
    EGLBoolean initialized = EGL_FALSE;
 
-   _eglSetLogProc(droid_log);
-
    loader_set_logger(_eglLog);
 
    if (!getenv("LIBGL_ALWAYS_SOFTWARE"))
