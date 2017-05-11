1b488786dcec8d5c7180ecbec36ef25dcbe7509a
Anuj Phogat <anuj.phogat@gmail.com>
2017-05-11 14:58:13 -0700

    i965/cnl: Add gen10 specific function declarations
    These declarations will help the code start compiling
    once we wire up the makefiles for gen10. Later patches
    will start using these functions for gen10.
    
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/intel/isl/isl_priv.h
+++ src/intel/isl/isl_priv.h
@@ -178,6 +178,10 @@ isl_gen9_surf_fill_state_s(const struct isl_device *dev, void *state,
                            const struct isl_surf_fill_state_info *restrict info);
 
 void
+isl_gen10_surf_fill_state_s(const struct isl_device *dev, void *state,
+                            const struct isl_surf_fill_state_info *restrict info);
+
+void
 isl_gen4_buffer_fill_state_s(void *state,
                              const struct isl_buffer_fill_state_info *restrict info);
 
@@ -206,6 +210,10 @@ isl_gen9_buffer_fill_state_s(void *state,
                              const struct isl_buffer_fill_state_info *restrict info);
 
 void
+isl_gen10_buffer_fill_state_s(void *state,
+                              const struct isl_buffer_fill_state_info *restrict info);
+
+void
 isl_gen4_emit_depth_stencil_hiz_s(const struct isl_device *dev, void *batch,
                                   const struct isl_depth_stencil_hiz_emit_info *restrict info);
 
@@ -232,5 +240,9 @@ isl_gen8_emit_depth_stencil_hiz_s(const struct isl_device *dev, void *batch,
 void
 isl_gen9_emit_depth_stencil_hiz_s(const struct isl_device *dev, void *batch,
                                   const struct isl_depth_stencil_hiz_emit_info *restrict info);
+
+void
+isl_gen10_emit_depth_stencil_hiz_s(const struct isl_device *dev, void *batch,
+                                   const struct isl_depth_stencil_hiz_emit_info *restrict info);
 
 #endif /* ISL_PRIV_H */
