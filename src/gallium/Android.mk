yuq/mesa-lima

commit a3d98ca62febdfbe035d655cb7c1f849bccfa105

Android: use symlinks for driver loading
Instead of having special driver loading logic for Android, create
symlinks to gallium_dri.so so we can use the standard loading logic.

Reviewed-by: Eric Anholt <eric@anholt.net>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Rob Herring <robh@kernel.org>

src/gallium/drivers/svga/Android.mk
@@ -40,5 +40,6 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_VMWGFX),)
+GALLIUM_TARGET_DRIVERS := vmwgfx
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_svga)
 endif

src/gallium/drivers/freedreno/Android.mk
@@ -50,6 +50,7 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_FREEDRENO),)
+GALLIUM_TARGET_DRIVERS += msm
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_freedreno)
 $(eval GALLIUM_SHARED_LIBS += $(LOCAL_SHARED_LIBRARIES))
 endif

src/gallium/drivers/i915/Android.mk
@@ -36,5 +36,6 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_I915),)
+GALLIUM_TARGET_DRIVERS += i915
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_i915)
 endif

src/gallium/drivers/nouveau/Android.mk
@@ -43,6 +43,7 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_NOUVEAU),)
+GALLIUM_TARGET_DRIVERS += nouveau
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_nouveau)
 $(eval GALLIUM_SHARED_LIBS += $(LOCAL_SHARED_LIBRARIES))
 endif

src/gallium/drivers/pl111/Android.mk
@@ -34,5 +34,6 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_PL111),)
+GALLIUM_TARGET_DRIVERS += pl111
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_pl111)
 endif

src/gallium/drivers/r300/Android.mk
@@ -42,6 +42,7 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_R300),)
+GALLIUM_TARGET_DRIVERS += r300
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_radeon)
 $(eval GALLIUM_SHARED_LIBS += $(LOCAL_SHARED_LIBRARIES))
 endif

src/gallium/drivers/r600/Android.mk
@@ -49,6 +49,7 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_R600),)
+GALLIUM_TARGET_DRIVERS += r600
 $(eval GALLIUM_LIBS += \
 	$(LOCAL_MODULE) \
 	libmesa_winsys_radeon)

src/gallium/drivers/radeonsi/Android.mk
@@ -49,6 +49,7 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_RADEONSI),)
+GALLIUM_TARGET_DRIVERS += radeonsi
 $(eval GALLIUM_LIBS += \
 	$(LOCAL_MODULE) \
 	$(LOCAL_STATIC_LIBRARIES) \

maurossi/mesa

commit 8cd9c0d2901b692b3ab7ab1b63a9b98bbe6a5a1f

Android: generate symlinks for all enabled gallium drivers
Current post install command relies on GALLIUM_TARGET_DRIVERS variable,
however variable needs to be initialized in src/gallium/Android.mk
in order that all enabled gallium drivers symlinks are correctly generated.

At the moment due to sorting of INC_DIRS and variable set with svga (vmwgfx)
only vmwgfx_dri.so and virtio_gpu_dri.so symlinks are generated.

Fixes: a3d98ca62f ("Android: use symlinks for driver loading")

src/gallium/Android.mk
@@ -25,6 +25,7 @@
 
 GALLIUM_TOP := $(call my-dir)
 GALLIUM_COMMON_MK := $(GALLIUM_TOP)/Android.common.mk
+GALLIUM_TARGET_DRIVERS :=
 
 SUBDIRS := auxiliary
 SUBDIRS += auxiliary/pipe-loader

src/gallium/drivers/svga/Android.mk
@@ -40,6 +40,6 @@ include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_VMWGFX),)
-GALLIUM_TARGET_DRIVERS := vmwgfx
+GALLIUM_TARGET_DRIVERS += vmwgfx
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_svga)
 endif