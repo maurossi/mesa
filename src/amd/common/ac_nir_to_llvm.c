e9ae1947b44a9e28674ae6b7675caf7e88239e08
Mauro Rossi <issor.oruam@gmail.com>
2017-08-03 02:24:33 +0200

    DO NOT MERGE: amd/common: ugly way to build w/o llvm 3.9
    HACK to workaround the drop of support of llvm 3.8
    and llvm 3.9 related changes in amd/common
    
    ATTENTION: randeonsi will not work with llvm 3.8 with these changes.
    This is just to avoid following errors and test mesa-dev source build.
    
    external/mesa/src/amd/common/ac_nir_to_llvm.c:3661:10:
    error: incompatible integer to pointer conversion assigning to 'LLVMValueRef'
    (aka 'struct LLVMOpaqueValue *') from 'int' [-Werror,-Wint-conversion]
                    result = LLVMBuildAtomicCmpXchg(ctx->builder,
                           ^ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    external/mesa/src/amd/common/ac_nir_to_llvm.c:6242:20:
    error: incompatible integer to pointer conversion initializing 'LLVMTargetDataRef'
    (aka 'struct LLVMOpaqueTargetData *') with an expression of type 'int' [-Werror,-Wint-conversion]
            LLVMTargetDataRef data_layout = LLVMCreateTargetDataLayout(tm);
                              ^             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/amd/common/ac_nir_to_llvm.c
+++ src/amd/common/ac_nir_to_llvm.c
@@ -3808,11 +3808,13 @@ static LLVMValueRef visit_var_atomic(struct nir_to_llvm_context *ctx,
 
 	if (instr->intrinsic == nir_intrinsic_var_atomic_comp_swap) {
 		LLVMValueRef src1 = get_src(ctx->nir, instr->src[1]);
+#if HAVE_LLVM >= 0x0309
 		result = LLVMBuildAtomicCmpXchg(ctx->builder,
 						ptr, src, src1,
 						LLVMAtomicOrderingSequentiallyConsistent,
 						LLVMAtomicOrderingSequentiallyConsistent,
 						false);
+#endif
 	} else {
 		LLVMAtomicRMWBinOp op;
 		switch (instr->intrinsic) {
@@ -6565,13 +6567,13 @@ LLVMModuleRef ac_translate_nir_to_llvm(LLVMTargetMachineRef tm,
 	ac_llvm_context_init(&ctx.ac, ctx.context, options->chip_class);
 	ctx.ac.module = ctx.module;
 	LLVMSetTarget(ctx.module, options->supports_spill ? "amdgcn-mesa-mesa3d" : "amdgcn--");
-
+#if HAVE_LLVM >= 0x0309
 	LLVMTargetDataRef data_layout = LLVMCreateTargetDataLayout(tm);
 	char *data_layout_str = LLVMCopyStringRepOfTargetData(data_layout);
 	LLVMSetDataLayout(ctx.module, data_layout_str);
 	LLVMDisposeTargetData(data_layout);
 	LLVMDisposeMessage(data_layout_str);
-
+#endif
 	setup_types(&ctx);
 	ctx.builder = LLVMCreateBuilderInContext(ctx.context);
 	ctx.ac.builder = ctx.builder;
