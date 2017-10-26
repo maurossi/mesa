05e3b801350c388869cb340b1c5ecded034888f2
Mauro Rossi <issor.oruam@gmail.com>
2017-10-27 00:00:42 +0200

    Android: move drivers' symlinks to /vendor
    Having moved gallium_dri.so library to /vendor/lib/dri
    also symlinks need to be coherently created using TARGET_OUT_VENDOR insted of TARGET_OUT
    or all non Intel drivers will not be loaded with Android N and earlier,
    thus causing SurfaceFlinger SIGABRT
    
    Fixes: c3f75d483c ("Android: move libraries to /vendor")
    
    Cc: 17.3 <mesa-stable@lists.freedesktop.org>

--- src/gallium/targets/dri/Android.mk
+++ src/gallium/targets/dri/Android.mk
@@ -70,8 +70,8 @@ LOCAL_SHARED_LIBRARIES += $(sort $(GALLIUM_SHARED_LIBS))
 ifneq ($(filter 5 6 7, $(MESA_ANDROID_MAJOR_VERSION)),)
 LOCAL_POST_INSTALL_CMD := \
 	$(foreach l, lib $(if $(filter true,$(TARGET_IS_64_BIT)),lib64), \
-	  mkdir -p $(TARGET_OUT)/$(l)/$(MESA_DRI_MODULE_REL_PATH); \
-	  $(foreach d, $(GALLIUM_TARGET_DRIVERS), ln -sf gallium_dri.so $(TARGET_OUT)/$(l)/$(MESA_DRI_MODULE_REL_PATH)/$(d)_dri.so;) \
+	  mkdir -p $(TARGET_OUT_VENDOR)/$(l)/$(MESA_DRI_MODULE_REL_PATH); \
+	  $(foreach d, $(GALLIUM_TARGET_DRIVERS), ln -sf gallium_dri.so $(TARGET_OUT_VENDOR)/$(l)/$(MESA_DRI_MODULE_REL_PATH)/$(d)_dri.so;) \
 	)
 else
 LOCAL_MODULE_SYMLINKS := $(foreach d, $(GALLIUM_TARGET_DRIVERS), $(d)_dri.so)
