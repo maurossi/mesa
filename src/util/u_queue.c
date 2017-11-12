7b573420319d88d983e94128f71be98c841e0451
Mauro Rossi <issor.oruam@gmail.com>
2017-11-12 01:36:09 +0100

    util/u_queue: use clock_gettime() instead of timespec_get()
    clock_gettime(CLOCK_REALTIME, &ts) is equivalent to timespec_get(&ts, TIME_UTC)
    but has the advantage that avoids the following building error when building with Android:
    
    external/mesa/src/util/u_queue.c:173: error: undefined reference to 'timespec_get'
    
    Fixes: e3a8013de8 ("util/u_queue: add util_queue_fence_wait_timeout")

--- src/util/u_queue.c
+++ src/util/u_queue.c
@@ -170,7 +170,7 @@ _util_queue_fence_wait_timeout(struct util_queue_fence *fence,
    if (rel > 0) {
       struct timespec ts;
 
-      timespec_get(&ts, TIME_UTC);
+      clock_gettime(CLOCK_REALTIME, &ts);
 
       ts.tv_sec += abs_timeout / (1000*1000*1000);
       ts.tv_nsec += abs_timeout % (1000*1000*1000);
