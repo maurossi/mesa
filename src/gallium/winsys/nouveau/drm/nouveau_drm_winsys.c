42e85c15a6a343e817f5d2abfb31a6fa49f8ce72
Emil Velikov <emil.l.velikov@gmail.com>
2018-09-28 14:48:49 +0100

    nouveau: remove nouveau_screen reuse (v2)
    The screen reuse was introduced to handle GL-VDPAU interop.
    Since we have a better solution in place, we can drop this workaround
    the respective exported symbol and symbol hacks.
    
    As we follow the same approach for other drivers this will allow us to
    remove the VL code from the dri modules and the odd hack that arise around
    LLVM use.
    
    Mauro, this should effectively fix the nouveau MT issues so you can drop
    the following patches:
    
     WIP nouveau: add locking (v6)
     nouveau: more locking - make sure that fence work is always done with the push mutex acquired (v2)
     nv30 locking fixes (v2)
     nv30 additional locking fix in context flush
    and
     add load_pipe_screen for drm_gralloc (v2)
    
    (v2) Fixes in src/gallium/drivers/nouveau/nouveau_screen.h due to 26ed0f0234
         ("nvc0: increase NOUVEAU_TRANSFER_PUSHBUF_THRESHOLD to 1024 on Kepler+")
    
    Cc: Mauro Rossi <issor.oruam@gmail.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/gallium/winsys/nouveau/drm/nouveau_drm_winsys.c
+++ src/gallium/winsys/nouveau/drm/nouveau_drm_winsys.c
@@ -17,84 +17,16 @@
 #include <nvif/class.h>
 #include <nvif/cl0080.h>
 
-static struct util_hash_table *fd_tab = NULL;
-
-static mtx_t nouveau_screen_mutex = _MTX_INITIALIZER_NP;
-
-bool nouveau_drm_screen_unref(struct nouveau_screen *screen)
-{
-	int ret;
-	if (screen->refcount == -1)
-		return true;
-
-	mtx_lock(&nouveau_screen_mutex);
-	ret = --screen->refcount;
-	assert(ret >= 0);
-	if (ret == 0)
-		util_hash_table_remove(fd_tab, intptr_to_pointer(screen->drm->fd));
-	mtx_unlock(&nouveau_screen_mutex);
-	return ret == 0;
-}
-
-static unsigned hash_fd(void *key)
-{
-    int fd = pointer_to_intptr(key);
-    struct stat stat;
-    fstat(fd, &stat);
-
-    return stat.st_dev ^ stat.st_ino ^ stat.st_rdev;
-}
-
-static int compare_fd(void *key1, void *key2)
-{
-    int fd1 = pointer_to_intptr(key1);
-    int fd2 = pointer_to_intptr(key2);
-    struct stat stat1, stat2;
-    fstat(fd1, &stat1);
-    fstat(fd2, &stat2);
-
-    return stat1.st_dev != stat2.st_dev ||
-           stat1.st_ino != stat2.st_ino ||
-           stat1.st_rdev != stat2.st_rdev;
-}
-
-PUBLIC struct pipe_screen *
+struct pipe_screen *
 nouveau_drm_screen_create(int fd)
 {
 	struct nouveau_drm *drm = NULL;
 	struct nouveau_device *dev = NULL;
 	struct nouveau_screen *(*init)(struct nouveau_device *);
 	struct nouveau_screen *screen = NULL;
-	int ret, dupfd;
+	int ret;
 
-	mtx_lock(&nouveau_screen_mutex);
-	if (!fd_tab) {
-		fd_tab = util_hash_table_create(hash_fd, compare_fd);
-		if (!fd_tab) {
-			mtx_unlock(&nouveau_screen_mutex);
-			return NULL;
-		}
-	}
-
-	screen = util_hash_table_get(fd_tab, intptr_to_pointer(fd));
-	if (screen) {
-		screen->refcount++;
-		mtx_unlock(&nouveau_screen_mutex);
-		return &screen->base;
-	}
-
-	/* Since the screen re-use is based on the device node and not the fd,
-	 * create a copy of the fd to be owned by the device. Otherwise a
-	 * scenario could occur where two screens are created, and the first
-	 * one is shut down, along with the fd being closed. The second
-	 * (identical) screen would now have a reference to the closed fd. We
-	 * avoid this by duplicating the original fd. Note that
-	 * nouveau_device_wrap does not close the fd in case of a device
-	 * creation error.
-	 */
-	dupfd = fcntl(fd, F_DUPFD_CLOEXEC, 3);
-
-	ret = nouveau_drm_new(dupfd, &drm);
+	ret = nouveau_drm_new(fd, &drm);
 	if (ret)
 		goto err;
 
@@ -137,13 +69,6 @@ nouveau_drm_screen_create(int fd)
 	if (!screen || !screen->base.context_create)
 		goto err;
 
-	/* Use dupfd in hash table, to avoid errors if the original fd gets
-	 * closed by its owner. The hash key needs to live at least as long as
-	 * the screen.
-	 */
-	util_hash_table_set(fd_tab, intptr_to_pointer(dupfd), screen);
-	screen->refcount = 1;
-	mtx_unlock(&nouveau_screen_mutex);
 	return &screen->base;
 
 err:
@@ -152,8 +77,6 @@ err:
 	} else {
 		nouveau_device_del(&dev);
 		nouveau_drm_del(&drm);
-		close(dupfd);
 	}
-	mtx_unlock(&nouveau_screen_mutex);
 	return NULL;
 }
