60b1293cb665973997902190db7d9cbe727370be
Karol Herbst <kherbst@redhat.com>
2018-10-07 16:05:55 +0200

    nv50,nvc0: simplify screen.fence
    Signed-off-by: Karol Herbst <kherbst@redhat.com>

--- src/gallium/drivers/nouveau/nv50/nv50_screen.c
+++ src/gallium/drivers/nouveau/nv50/nv50_screen.c
@@ -580,7 +580,8 @@ nv50_screen_fence_emit(struct pipe_screen *pscreen, u32 *sequence)
 static u32
 nv50_screen_fence_update(struct pipe_screen *pscreen)
 {
-   return nv50_screen(pscreen)->fence.map[0];
+   uint32_t *map = nv50_screen(pscreen)->fence.bo->map;
+   return map[0];
 }
 
 static void
@@ -988,7 +989,6 @@ nv50_screen_create(struct nouveau_device *dev)
    }
 
    nouveau_bo_map(screen->fence.bo, 0, NULL);
-   screen->fence.map = screen->fence.bo->map;
    screen->base.fence.emit = nv50_screen_fence_emit;
    screen->base.fence.update = nv50_screen_fence_update;
 
