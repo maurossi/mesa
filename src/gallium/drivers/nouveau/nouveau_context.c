479b2872a86f175b244d9fddc7a1477a63505db6
Karol Herbst <kherbst@redhat.com>
2018-10-12 21:58:11 +0200

    WIP: per context fences/pushbuf

--- src/gallium/drivers/nouveau/nouveau_context.c
+++ src/gallium/drivers/nouveau/nouveau_context.c
@@ -12,8 +12,39 @@ nouveau_set_debug_callback(struct pipe_context *pipe,
       memset(&context->debug, 0, sizeof(context->debug));
 }
 
-void
+int
 nouveau_context_init(struct nouveau_context *context)
 {
+   struct nouveau_screen *screen = context->screen;
+   int ret;
+
    context->pipe.set_debug_callback = nouveau_set_debug_callback;
+
+   ret = nouveau_client_new(screen->device, &context->client);
+   if (ret)
+      return ret;
+
+   ret = nouveau_pushbuf_new(context->client, screen->channel,
+                             4, 512 * 1024, 1,
+                             &context->pushbuf);
+   if (ret)
+      return ret;
+
+   nouveau_fence_list_init(&context->fence, screen, context->pushbuf);
+   return ret;
+}
+
+void
+nouveau_context_destroy(struct nouveau_context *ctx)
+{
+   int i;
+
+   for (i = 0; i < NOUVEAU_MAX_SCRATCH_BUFS; ++i)
+      if (ctx->scratch.bo[i])
+         nouveau_bo_ref(NULL, &ctx->scratch.bo[i]);
+
+   nouveau_pushbuf_del(&ctx->pushbuf);
+   nouveau_client_del(&ctx->client);
+
+   FREE(ctx);
 }
