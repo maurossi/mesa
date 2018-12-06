74e2247af682d257a9850d4c86357931c07cb64e
Mauro Rossi <issor.oruam@gmail.com>
2018-12-06 20:07:00 +0100

    android: radv: enable Vulkan 1.1.70
    radv_extension.py changes to enable Vulkan 1.1.70 in Android 8.1 (O)
    Android 9.0 (P) requires VK_KHR_sampler_ycbcr_conversion, not available yet

--- src/amd/vulkan/radv_extensions.py
+++ src/amd/vulkan/radv_extensions.py
@@ -291,7 +291,7 @@ VkResult radv_EnumerateInstanceVersion(
 uint32_t
 radv_physical_device_api_version(struct radv_physical_device *dev)
 {
-    if (!ANDROID && dev->rad_info.has_syncobj_wait_for_submit)
+    if (dev->rad_info.has_syncobj_wait_for_submit)
         return VK_MAKE_VERSION(1, 1, 70);
     return VK_MAKE_VERSION(1, 0, 68);
 }
