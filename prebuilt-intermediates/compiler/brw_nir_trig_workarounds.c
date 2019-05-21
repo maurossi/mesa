d638e1b23427b09aee79481cbd919bb8bf5c4ec1
John Stultz <john.stultz@linaro.org>
2019-05-21 01:27:48 +0000

    mesa: prebuilt-intermediates: Fix/update brw_nir_trig_workarounds.c source
    So when doing the 19.0.3 merge, I apparently didn't commit the
    changes to brw_nir_trig_workarounds.c, so we ended up with what
    we had before.
    
    Now that we have the script to generate the prebuilts, commit
    the output of that, so we're synced.
    
    Change-Id: Id528d2954f291ffa59b775d9b8218acdc5004d89
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- prebuilt-intermediates/compiler/brw_nir_trig_workarounds.c
+++ prebuilt-intermediates/compiler/brw_nir_trig_workarounds.c
@@ -1,6 +1,7 @@
 #include "brw_nir.h"
 
 #include "nir.h"
+#include "nir_builder.h"
 #include "nir_search.h"
 #include "nir_search_helpers.h"
 
@@ -16,15 +17,57 @@ struct transform {
 #endif
 
    
+static const nir_search_variable search0_0 = {
+   { nir_search_value_variable, -1 },
+   0, /* x */
+   false,
+   nir_type_invalid,
+   NULL,
+};
+static const nir_search_expression search0 = {
+   { nir_search_value_expression, -1 },
+   false,
+   nir_op_fsin,
+   { &search0_0.value },
+   NULL,
+};
+   
+static const nir_search_variable replace0_0_0 = {
+   { nir_search_value_variable, -1 },
+   0, /* x */
+   false,
+   nir_type_invalid,
+   NULL,
+};
+static const nir_search_expression replace0_0 = {
+   { nir_search_value_expression, -1 },
+   false,
+   nir_op_fsin,
+   { &replace0_0_0.value },
+   NULL,
+};
+
+static const nir_search_constant replace0_1 = {
+   { nir_search_value_constant, -1 },
+   nir_type_float, { 0x3fefffc115df6556 /* 0.99997 */ },
+};
+static const nir_search_expression replace0 = {
+   { nir_search_value_expression, -1 },
+   false,
+   nir_op_fmul,
+   { &replace0_0.value, &replace0_1.value },
+   NULL,
+};
+   
 static const nir_search_variable search1_0 = {
-   { nir_search_value_variable, 0 },
+   { nir_search_value_variable, -1 },
    0, /* x */
    false,
    nir_type_invalid,
    NULL,
 };
 static const nir_search_expression search1 = {
-   { nir_search_value_expression, 0 },
+   { nir_search_value_expression, -1 },
    false,
    nir_op_fcos,
    { &search1_0.value },
@@ -32,14 +75,14 @@ static const nir_search_expression search1 = {
 };
    
 static const nir_search_variable replace1_0_0 = {
-   { nir_search_value_variable, 0 },
+   { nir_search_value_variable, -1 },
    0, /* x */
    false,
    nir_type_invalid,
    NULL,
 };
 static const nir_search_expression replace1_0 = {
-   { nir_search_value_expression, 0 },
+   { nir_search_value_expression, -1 },
    false,
    nir_op_fcos,
    { &replace1_0_0.value },
@@ -47,11 +90,11 @@ static const nir_search_expression replace1_0 = {
 };
 
 static const nir_search_constant replace1_1 = {
-   { nir_search_value_constant, 0 },
+   { nir_search_value_constant, -1 },
    nir_type_float, { 0x3fefffc115df6556 /* 0.99997 */ },
 };
 static const nir_search_expression replace1 = {
-   { nir_search_value_expression, 0 },
+   { nir_search_value_expression, -1 },
    false,
    nir_op_fmul,
    { &replace1_0.value, &replace1_1.value },
@@ -61,56 +104,13 @@ static const nir_search_expression replace1 = {
 static const struct transform brw_nir_apply_trig_workarounds_fcos_xforms[] = {
    { &search1, &replace1.value, 0 },
 };
-   
-static const nir_search_variable search0_0 = {
-   { nir_search_value_variable, 0 },
-   0, /* x */
-   false,
-   nir_type_invalid,
-   NULL,
-};
-static const nir_search_expression search0 = {
-   { nir_search_value_expression, 0 },
-   false,
-   nir_op_fsin,
-   { &search0_0.value },
-   NULL,
-};
-   
-static const nir_search_variable replace0_0_0 = {
-   { nir_search_value_variable, 0 },
-   0, /* x */
-   false,
-   nir_type_invalid,
-   NULL,
-};
-static const nir_search_expression replace0_0 = {
-   { nir_search_value_expression, 0 },
-   false,
-   nir_op_fsin,
-   { &replace0_0_0.value },
-   NULL,
-};
-
-static const nir_search_constant replace0_1 = {
-   { nir_search_value_constant, 0 },
-   nir_type_float, { 0x3fefffc115df6556 /* 0.99997 */ },
-};
-static const nir_search_expression replace0 = {
-   { nir_search_value_expression, 0 },
-   false,
-   nir_op_fmul,
-   { &replace0_0.value, &replace0_1.value },
-   NULL,
-};
-
 static const struct transform brw_nir_apply_trig_workarounds_fsin_xforms[] = {
    { &search0, &replace0.value, 0 },
 };
 
 static bool
-brw_nir_apply_trig_workarounds_block(nir_block *block, const bool *condition_flags,
-                   void *mem_ctx)
+brw_nir_apply_trig_workarounds_block(nir_builder *build, nir_block *block,
+                   const bool *condition_flags)
 {
    bool progress = false;
 
@@ -127,8 +127,7 @@ brw_nir_apply_trig_workarounds_block(nir_block *block, const bool *condition_fla
          for (unsigned i = 0; i < ARRAY_SIZE(brw_nir_apply_trig_workarounds_fcos_xforms); i++) {
             const struct transform *xform = &brw_nir_apply_trig_workarounds_fcos_xforms[i];
             if (condition_flags[xform->condition_offset] &&
-                nir_replace_instr(alu, xform->search, xform->replace,
-                                  mem_ctx)) {
+                nir_replace_instr(build, alu, xform->search, xform->replace)) {
                progress = true;
                break;
             }
@@ -138,8 +137,7 @@ brw_nir_apply_trig_workarounds_block(nir_block *block, const bool *condition_fla
          for (unsigned i = 0; i < ARRAY_SIZE(brw_nir_apply_trig_workarounds_fsin_xforms); i++) {
             const struct transform *xform = &brw_nir_apply_trig_workarounds_fsin_xforms[i];
             if (condition_flags[xform->condition_offset] &&
-                nir_replace_instr(alu, xform->search, xform->replace,
-                                  mem_ctx)) {
+                nir_replace_instr(build, alu, xform->search, xform->replace)) {
                progress = true;
                break;
             }
@@ -156,16 +154,23 @@ brw_nir_apply_trig_workarounds_block(nir_block *block, const bool *condition_fla
 static bool
 brw_nir_apply_trig_workarounds_impl(nir_function_impl *impl, const bool *condition_flags)
 {
-   void *mem_ctx = ralloc_parent(impl);
    bool progress = false;
 
+   nir_builder build;
+   nir_builder_init(&build, impl);
+
    nir_foreach_block_reverse(block, impl) {
-      progress |= brw_nir_apply_trig_workarounds_block(block, condition_flags, mem_ctx);
+      progress |= brw_nir_apply_trig_workarounds_block(&build, block, condition_flags);
    }
 
-   if (progress)
+   if (progress) {
       nir_metadata_preserve(impl, nir_metadata_block_index |
                                   nir_metadata_dominance);
+    } else {
+#ifndef NDEBUG
+      impl->valid_metadata &= ~nir_metadata_not_properly_reset;
+#endif
+    }
 
    return progress;
 }
