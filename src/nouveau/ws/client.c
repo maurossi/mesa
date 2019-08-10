128243a7485bd44ae666cf1e15d03a70e4b9be7b
Karol Herbst <kherbst@redhat.com>
2019-08-10 22:00:50 +0200

    nouveau: add libwsnouveau

---
+++ src/nouveau/ws/client.c
@@ -0,0 +1,15 @@
+#include "priv.h"
+
+int
+nouveau_ws_client_new(struct nouveau_ws_device *dev,
+                      struct nouveau_ws_client **client)
+{
+   assert(false);
+   return -1;
+}
+
+void
+nouveau_ws_client_del(struct nouveau_ws_client **client)
+{
+   assert(false);
+}
