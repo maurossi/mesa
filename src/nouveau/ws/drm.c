ec629df4b908c4186a75bcb171871b008ec172f3
Karol Herbst <kherbst@redhat.com>
2019-08-10 22:00:50 +0200

    nouveau: add libwsnouveau

---
+++ src/nouveau/ws/drm.c
@@ -0,0 +1,15 @@
+#include "priv.h"
+
+int
+nouveau_ws_drm_new(int fd,
+                   struct nouveau_ws_drm **drm)
+{
+   assert(false);
+   return -1;
+}
+
+void
+nouveau_ws_drm_del(struct nouveau_ws_drm **drm)
+{
+   assert(false);
+}
---
+++ src/nouveau/ws/param.c
@@ -0,0 +1,10 @@
+#include "priv.h"
+
+int
+nouveau_ws_getparam(struct nouveau_ws_device *dev,
+                    uint64_t param,
+                    uint64_t *value)
+{
+   assert(false);
+   return -1;
+}
