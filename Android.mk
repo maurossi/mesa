fc12c72187de6caae51bb181ce58bf760f232a39
Mauro Rossi <issor.oruam@gmail.com>
2020-02-21 11:13:57 +0100

    Android: use host python explicitly (v2)
    The prebuilt python in AOSP can't use the python modules in the host
    since Android 10. However, mesa needs some modules like mako.template
    which are not available in AOSP's tree.
    
    To overcome that, use the host python directly.
    
    (v2) update after commit cad400a59e47
    "freedreno: android: fix build failure on android due to python version"

--- Android.mk
+++ Android.mk
@@ -42,8 +42,8 @@ MESA_DRI_MODULE_UNSTRIPPED_PATH := $(TARGET_OUT_SHARED_LIBRARIES_UNSTRIPPED)/$(M
 MESA_DRI_LDFLAGS := -Wl,--build-id=sha1
 
 MESA_COMMON_MK := $(MESA_TOP)/Android.common.mk
-MESA_PYTHON2 := python
-MESA_PYTHON3 := python3
+MESA_PYTHON2 := /usr/bin/python
+MESA_PYTHON3 := /usr/bin/python3
 
 # Lists to convert driver names to boolean variables
 # in form of <driver name>.<boolean make variable>
