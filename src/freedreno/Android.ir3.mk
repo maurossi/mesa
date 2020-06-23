Grima04/mesa-turnip-kgsl

commit 1e8808a4a0f7f28dfc885bbe6c50e7a65ad15bbf

freedreno/ir3: refactor out helper to compile shader from asm
Deduplicate a bit of hand-building of ir3_shader/_variant from
computerator and delay test.  This also removes the need for
external things to depend on generated ir3_parser header.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5508>

src/freedreno/Makefile.sources
@@ -22,6 +22,8 @@ ir3_SOURCES := \
 	ir3/ir3.c \
 	ir3/ir3_a4xx.c \
 	ir3/ir3_a6xx.c \
+	ir3/ir3_assembler.c \
+	ir3/ir3_assembler.h \
 	ir3/ir3_compiler.c \
 	ir3/ir3_compiler.h \
 	ir3/ir3_compiler_nir.c \

src/freedreno/computerator/meson.build
@@ -23,7 +23,6 @@ computerator_files = [
   'ir3_asm.c',
   'main.c',
   freedreno_xml_header_files,
-  ir3_parser,
 ]
 
 computerator = executable(

src/freedreno/ir3/ir3_assembler.c
@@ -21,41 +21,42 @@
  * SOFTWARE.
  */
 
-#include "ir3/ir3_assembler.h"
-#include "ir3/ir3_compiler.h"
+#include <err.h>
 
-#include "ir3_asm.h"
+#include "ir3_assembler.h"
+#include "ir3_compiler.h"
+#include "ir3_parser.h"
 
-struct ir3_kernel *
-ir3_asm_assemble(struct ir3_compiler *c, FILE *in)
+/**
+ * A helper to go from ir3 assembly to assembled shader.  The shader has a
+ * single variant.
+ */
+struct ir3_shader *
+ir3_parse_asm(struct ir3_compiler *c, struct ir3_kernel_info *info, FILE *in)
 {
-	struct ir3_kernel *kernel = calloc(1, sizeof(*kernel));
-	struct ir3_shader *shader = ir3_parse_asm(c, &kernel->info, in);
-	struct ir3_shader_variant *v = shader->variants;
+	struct ir3_shader *shader = calloc(1, sizeof(*shader));
+	shader->compiler = c;
+	shader->type = MESA_SHADER_COMPUTE;
+	mtx_init(&shader->variants_lock, mtx_plain);
 
-	v->mergedregs = true;
+	struct ir3_shader_variant *v = calloc(1, sizeof(*v));
+	v->type = MESA_SHADER_COMPUTE;
+	v->shader = shader;
 
-	kernel->v = v;
-	kernel->bin = v->bin;
+	shader->variants = v;
+	shader->variant_count = 1;
 
-	memcpy(kernel->base.local_size, kernel->info.local_size, sizeof(kernel->base.local_size));
-	kernel->base.num_bufs = kernel->info.num_bufs;
-	memcpy(kernel->base.buf_sizes, kernel->info.buf_sizes, sizeof(kernel->base.buf_sizes));
+	info->numwg = INVALID_REG;
 
-	unsigned sz = v->info.sizedwords * 4;
+	v->ir = ir3_parse(v, info, in);
+	if (!v->ir)
+		errx(-1, "parse failed");
 
-	v->bo = fd_bo_new(c->dev, sz,
-			DRM_FREEDRENO_GEM_CACHE_WCOMBINE |
-			DRM_FREEDRENO_GEM_TYPE_KMEM,
-			"%s", ir3_shader_stage(v));
+	ir3_debug_print(v->ir, "AFTER PARSING");
 
-	memcpy(fd_bo_map(v->bo), kernel->bin, sz);
+	v->bin = ir3_shader_assemble(v);
+	if (!v->bin)
+		errx(-1, "assembler failed");
 
-	return kernel;
-}
-
-void
-ir3_asm_disassemble(struct ir3_kernel *k, FILE *out)
-{
-	ir3_shader_disasm(k->v, k->bin, out);
+	return shader;
 }

src/freedreno/ir3/ir3_assembler.h
@@ -21,41 +21,26 @@
  * SOFTWARE.
  */
 
-#include "ir3/ir3_assembler.h"
-#include "ir3/ir3_compiler.h"
+#ifndef __IR3_ASSEMBLER_H__
+#define __IR3_ASSEMBLER_H__
 
-#include "ir3_asm.h"
+#include <stdint.h>
+#include <stdio.h>
 
-struct ir3_kernel *
-ir3_asm_assemble(struct ir3_compiler *c, FILE *in)
-{
-	struct ir3_kernel *kernel = calloc(1, sizeof(*kernel));
-	struct ir3_shader *shader = ir3_parse_asm(c, &kernel->info, in);
-	struct ir3_shader_variant *v = shader->variants;
+#define MAX_BUFS 4
 
-	v->mergedregs = true;
+struct ir3_kernel_info {
+	uint32_t local_size[3];
+	uint32_t num_bufs;
+	uint32_t buf_sizes[MAX_BUFS]; /* size in dwords */
 
-	kernel->v = v;
-	kernel->bin = v->bin;
+	/* driver-param uniforms: */
+	unsigned numwg;
+};
 
-	memcpy(kernel->base.local_size, kernel->info.local_size, sizeof(kernel->base.local_size));
-	kernel->base.num_bufs = kernel->info.num_bufs;
-	memcpy(kernel->base.buf_sizes, kernel->info.buf_sizes, sizeof(kernel->base.buf_sizes));
+struct ir3_shader;
+struct ir3_compiler;
 
-	unsigned sz = v->info.sizedwords * 4;
+struct ir3_shader * ir3_parse_asm(struct ir3_compiler *c, struct ir3_kernel_info *info, FILE *in);
 
-	v->bo = fd_bo_new(c->dev, sz,
-			DRM_FREEDRENO_GEM_CACHE_WCOMBINE |
-			DRM_FREEDRENO_GEM_TYPE_KMEM,
-			"%s", ir3_shader_stage(v));
-
-	memcpy(fd_bo_map(v->bo), kernel->bin, sz);
-
-	return kernel;
-}
-
-void
-ir3_asm_disassemble(struct ir3_kernel *k, FILE *out)
-{
-	ir3_shader_disasm(k->v, k->bin, out);
-}
+#endif /* __IR3_ASSEMBLER_H__ */

src/freedreno/ir3/ir3_parser.y
@@ -22,17 +22,7 @@
  */
 
 %code requires {
-
-#define MAX_BUFS 4
-
-struct ir3_kernel_info {
-	uint32_t local_size[3];
-	uint32_t num_bufs;
-	uint32_t buf_sizes[MAX_BUFS]; /* size in dwords */
-
-	/* driver-param uniforms: */
-	unsigned numwg;
-};
+#include "ir3/ir3_assembler.h"
 
 struct ir3 * ir3_parse(struct ir3_shader_variant *v,
 		struct ir3_kernel_info *k, FILE *f);

src/freedreno/ir3/meson.build
@@ -66,6 +66,8 @@ libfreedreno_ir3_files = files(
   'ir3.c',
   'ir3_a4xx.c',
   'ir3_a6xx.c',
+  'ir3_assembler.c',
+  'ir3_assembler.h',
   'ir3_compiler_nir.c',
   'ir3_compiler.c',
   'ir3_compiler.h',
@@ -126,9 +128,9 @@ test('ir3_disasm',
 test('ir3_delay_test',
   executable(
     'ir3_delay_test',
-    ['tests/delay.c', ir3_parser],
+    'tests/delay.c',
     link_with: libfreedreno_ir3,
-    dependencies: [idep_mesautil, idep_nir_headers],
+    dependencies: [idep_mesautil, idep_nir],
     include_directories: [inc_freedreno, inc_include, inc_src, inc_mesa, inc_gallium],
   ),
   suite: ['freedreno'],

src/freedreno/ir3/tests/delay.c
@@ -24,8 +24,8 @@
 #include <stdio.h>
 
 #include "ir3.h"
+#include "ir3_assembler.h"
 #include "ir3_compiler.h"
-#include "ir3_parser.h"
 
 /*
  * A test for delay-slot calculation.  Each test specifies ir3 assembly
@@ -85,25 +85,16 @@ static const struct test {
 	),
 };
 
-static struct ir3 *
+static struct ir3_shader *
 parse_asm(struct ir3_compiler *c, const char *asmstr)
 {
-	struct ir3_shader shader = {
-		.type = MESA_SHADER_COMPUTE,
-		.compiler = c,
-	};
-	struct ir3_shader_variant v = {
-		.type = shader.type,
-		.shader = &shader,
-	};
 	struct ir3_kernel_info info = {};
 	FILE *in = fmemopen((void *)asmstr, strlen(asmstr), "r");
-
-	struct ir3 *ir = ir3_parse(&v, &info, in);
+	struct ir3_shader *shader = ir3_parse_asm(c, &info, in);
 
 	fclose(in);
 
-	return ir;
+	return shader;
 }
 
 static unsigned
@@ -190,9 +181,8 @@ main(int argc, char **argv)
 
 	for (int i = 0; i < ARRAY_SIZE(tests); i++) {
 		const struct test *test = &tests[i];
-		struct ir3 *ir = parse_asm(c, test->asmstr);
-
-		ir3_debug_print(ir, "AFTER PARSING");
+		struct ir3_shader *shader = parse_asm(c, test->asmstr);
+		struct ir3 *ir = shader->variants->ir;
 
 		regs_to_ssa(ir);
 
@@ -224,6 +214,8 @@ main(int argc, char **argv)
 		} else {
 			printf("%d: PASS\n", i);
 		}
+
+		ir3_shader_destroy(shader);
 	}
 
 	return result;

maurossi/mesa

commit dfb154612c866547b7ed1901c39d86d152ac23e5

android: freedreno/ir3: add missing generated sources and rules
Changelog:
- Makefile.sources: add ir3_lexer.c and ir3_parser.{c,h} generated sources
- Android.ir3.mk: add the necessary generated sources rules
- Android.ir3.mk: add the necessary include paths
- src/gallium/drivers/freedreno/Android.gen.mk: generate only ir3_nir_{imul,trig}.c for the moment

Fixes the following building error:

target  C: libfreedreno_ir3 <= external/mesa/src/freedreno/ir3/ir3_assembler.c
FAILED: out/target/product/x86_64/obj/STATIC_LIBRARIES/libfreedreno_ir3_intermediates/ir3/ir3_assembler.o
...
external/mesa/src/freedreno/ir3/ir3_assembler.c:28:10: fatal error: 'ir3_parser.h' file not found
         ^~~~~~~~~~~~~~
1 error generated.

Fixes: 1e8808a4a0f ("freedreno/ir3: refactor out helper to compile shader from asm")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Acked-by: Rob Clark <robdclark@gmail.com>
Acked-by: Eric Engestrom <eric@engestrom.ch>

src/freedreno/Android.ir3.mk
@@ -31,21 +31,51 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	$(ir3_SOURCES)
 
+LOCAL_MODULE := libfreedreno_ir3
+
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+intermediates := $(call local-generated-sources-dir)
+
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/compiler/nir \
 	$(MESA_TOP)/src/gallium/include \
 	$(MESA_TOP)/src/gallium/auxiliary \
 	$(MESA_TOP)/prebuilt-intermediates/nir \
+	$(MESA_TOP)/src/freedreno/ir3 \
+	$(intermediates)/ir3
 
 # We need libmesa_nir to get NIR's generated include directories.
 LOCAL_STATIC_LIBRARIES := \
 	libmesa_nir
 
-LOCAL_MODULE := libfreedreno_ir3
-
 LOCAL_GENERATED_SOURCES := \
 	$(MESA_GEN_GLSL_H) \
 	$(MESA_GEN_NIR_H)
+
+LOCAL_GENERATED_SOURCES += $(addprefix $(intermediates)/, \
+	ir3/ir3_lexer.c ir3/ir3_parser.c ir3/ir3_parser.h)
+
+ir3_lexer_deps := \
+	$(MESA_TOP)/src/freedreno/ir3/ir3_lexer.l
+
+ir3_parser_deps := \
+	$(MESA_TOP)/src/freedreno/ir3/ir3_parser.y
+
+$(intermediates)/ir3/ir3_lexer.c: $(ir3_lexer_deps)
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) flex -o $@ $<
+
+$(intermediates)/ir3/ir3_parser.c: $(ir3_parser_deps)
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) bison $< --name-prefix=ir3_yy --output=$@
+
+$(intermediates)/ir3/ir3_parser.h: $(ir3_parser_deps)
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) bison $< --name-prefix=ir3_yy --defines=$@
 
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)

src/freedreno/Makefile.sources
@@ -61,8 +61,11 @@ ir3_SOURCES := \
 	ir3/ir3_validate.c
 
 ir3_GENERATED_FILES := \
+	ir3/ir3_lexer.c \
 	ir3/ir3_nir_trig.c \
-	ir3/ir3_nir_imul.c
+	ir3/ir3_nir_imul.c \
+	ir3/ir3_parser.c \
+	ir3/ir3_parser.h
 
 perfcntrs_SOURCES := \
 	perfcntrs/fd2_perfcntr.c \

src/gallium/drivers/freedreno/Android.gen.mk
@@ -43,4 +43,4 @@ $(intermediates)/ir3/ir3_nir_imul.c: $(ir3_nir_imul_deps)
 	$(hide) $(MESA_PYTHON2) $< -p $(MESA_TOP)/src/compiler/nir > $@
 
 LOCAL_GENERATED_SOURCES += $(addprefix $(intermediates)/, \
-	$(ir3_GENERATED_FILES))
+	ir3/ir3_nir_trig.c ir3/ir3_nir_imul.c)