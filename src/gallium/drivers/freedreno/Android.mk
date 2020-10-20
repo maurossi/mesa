42e5ba9413a0383b649b21d7afb81d3559b5fe6c
Marijn Suijten <marijns95@gmail.com>
2020-10-20 21:17:12 +0200

    android: gallium/freedreno: Include libsync.h deps through libmesa_util
    libmesa_util provides libsync.h as well as the right include directory
    for the transitive dependency on android/sync.h:
    
        In file included from external/mesa3d/src/gallium/drivers/freedreno/a5xx/fd5_emit.c:34:
        In file included from external/mesa3d/src/gallium/drivers/freedreno/freedreno_resource.h:35:
        In file included from external/mesa3d/src/gallium/drivers/freedreno/freedreno_batch.h:328:
        In file included from external/mesa3d/src/gallium/drivers/freedreno/freedreno_context.h:33:
        external/mesa3d/src/util/libsync.h:48:10: fatal error: 'android/sync.h' file not found
        #include <android/sync.h>
                 ^~~~~~~~~~~~~~~~
    
    Fixes: e3c330c7b21 ("freedreno: Use Android's libsync instead of libdrm's.")
    Acked-by: Eric Anholt <eric@anholt.net>

--- src/gallium/drivers/freedreno/Android.mk
+++ src/gallium/drivers/freedreno/Android.mk
@@ -45,7 +45,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_GENERATED_SOURCES := $(MESA_GEN_NIR_H)
 
 LOCAL_SHARED_LIBRARIES := libdrm
-LOCAL_STATIC_LIBRARIES := libmesa_glsl libmesa_nir libfreedreno_drm libfreedreno_ir3 libfreedreno_perfcntrs libfreedreno_registers
+LOCAL_STATIC_LIBRARIES := libmesa_glsl libmesa_util libmesa_nir libfreedreno_drm libfreedreno_ir3 libfreedreno_perfcntrs libfreedreno_registers
 LOCAL_MODULE := libmesa_pipe_freedreno
 
 include $(GALLIUM_COMMON_MK)
