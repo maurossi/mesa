19a48412834c01c925143ae9c2b21597a485b10f
Mauro Rossi <issor.oruam@gmail.com>
2020-11-14 15:21:53 +0100

    Android: use host python explicitly
    To avoid the following building errors:
    
    FAILED: out/target/product/x86_64/gen/STATIC_LIBRARIES/libmesa_radv_common_intermediates/radv_entrypoints.c
    /bin/bash -c "python external/mesa/src/amd/vulkan/radv_entrypoints_gen.py --xml external/mesa/src/vulkan/registry/vk.xml	-
    -outdir out/target/product/x86_64/gen/STATIC_LIBRARIES/libmesa_radv_common_intermediates/"
    Traceback (most recent call last):
      File "external/mesa/src/amd/vulkan/radv_entrypoints_gen.py", line 29, in <module>
        import xml.etree.cElementTree as et
      File "/home/utente/r-x86_kernel/prebuilts/build-tools/linux-x86/bin/py2-cmd/internal/stdlib/xml/etree/cElementTree.py", line 3, in <module>
    ImportError: No module named _elementtree

--- Android.mk
+++ Android.mk
@@ -51,7 +51,7 @@ MESA_DRI_MODULE_UNSTRIPPED_PATH := $(TARGET_OUT_SHARED_LIBRARIES_UNSTRIPPED)/$(M
 MESA_DRI_LDFLAGS := -Wl,--build-id=sha1
 
 MESA_COMMON_MK := $(MESA_TOP)/Android.common.mk
-MESA_PYTHON2 := python
+MESA_PYTHON2 := /usr/bin/python
 
 # Lists to convert driver names to boolean variables
 # in form of <driver name>.<boolean make variable>
