4de570607cc3ef8ea9a453755283e14be60e1534
hmtheboy154 <buingoc67@gmail.com>
2021-06-15 21:51:44 +0700

    crocus : Bringing Android.mk support
    To compile with older version of AOSP (in my case 7.1), I decided to port it Android.mk by myself. Bringing the files from the old crocus Pull Request with some editing.
    
    https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11146/diffs?commit_id=2fcf12abb8b8be32bd288ea7e1423b54d74547a5#f215ff81f23ff8238fd1b82f54517d5416ed478e

--- android/Android.mk
+++ android/Android.mk
@@ -57,7 +57,7 @@ LOCAL_SHARED_LIBRARIES += libvulkan
 MESON_GEN_PKGCONFIGS += vulkan
 endif
 
-ifneq ($(filter iris,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
+ifneq ($(filter iris crocus,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
 LOCAL_SHARED_LIBRARIES += libdrm_intel
 MESON_GEN_PKGCONFIGS += libdrm_intel:$(LIBDRM_VERSION)
 endif
