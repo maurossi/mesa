5548001d72d84ee1babd2a93123e1fa4c68b5bfe
hmtheboy154 <buingoc67@gmail.com>
2021-06-15 22:00:31 +0700

    Android.mk : adding crocus to the build

--- Android.mk
+++ Android.mk
@@ -30,7 +30,7 @@ else
 # BOARD_GPU_DRIVERS should be defined.  The valid values are
 #
 #   classic drivers: i915 i965
-#   gallium drivers: swrast freedreno i915g nouveau kmsro r300g r600g radeonsi vc4 virgl vmwgfx etnaviv iris lima panfrost
+#   gallium drivers: swrast freedreno i915g nouveau kmsro r300g r600g radeonsi vc4 virgl vmwgfx etnaviv iris lima panfrost crocus
 #
 # The main target is libGLES_mesa.  For each classic driver enabled, a DRI
 # module will also be built.  DRI modules will be loaded by libGLES_mesa.
@@ -74,7 +74,8 @@ gallium_drivers := \
 	etnaviv.HAVE_GALLIUM_ETNAVIV \
 	iris.HAVE_GALLIUM_IRIS \
 	lima.HAVE_GALLIUM_LIMA \
-	panfrost.HAVE_GALLIUM_PANFROST
+	panfrost.HAVE_GALLIUM_PANFROST \
+	crocus.HAVE_GALLIUM_CROCUS
 
 ifeq ($(BOARD_GPU_DRIVERS),all)
 MESA_BUILD_CLASSIC := $(filter HAVE_%, $(subst ., , $(classic_drivers)))
