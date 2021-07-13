2e5031913426128726b505879838d280a71bf5cd
youling257 <youling257@gmail.com>
2021-07-13 23:24:42 +0800

    Revert "panfrost: Specialize blendable_formats for v6"
    This reverts commit d8eef119fd8b4d29b4a299e5a2383b6606503402.

--- src/panfrost/lib/pan_util.c
+++ src/panfrost/lib/pan_util.c
@@ -75,9 +75,12 @@ unsigned
 panfrost_format_to_bifrost_blend(const struct panfrost_device *dev,
                                  enum pipe_format format)
 {
-        mali_pixel_format pixfmt = (dev->quirks & HAS_SWIZZLES) ?
-                panfrost_blendable_formats_v7[format].bifrost :
-                panfrost_blendable_formats_v6[format].bifrost;
+        mali_pixel_format pixfmt = panfrost_blendable_formats_v7[format].bifrost;
 
-        return pixfmt ?: dev->formats[format].hw;
+        if (pixfmt) {
+                return pixfmt | ((dev->quirks & HAS_SWIZZLES) ?
+                                panfrost_get_default_swizzle(4) : 0);
+        } else {
+                return dev->formats[format].hw;
+        }
 }
