236d5dc8638b2a1215d0f5cb8acaa09c2d21ee05
Dave Airlie <airlied@redhat.com>
2022-06-11 06:17:09 +1000

    HACK: nouveau/codegen: split the tgsi/gallium pieces out of codegen.
    This allows it to be used in vulkan

---
+++ src/nouveau/codegen/nv50_ir_from_notgsi.cpp
@@ -0,0 +1,12 @@
+
+#include "nv50_ir.h"
+namespace nv50_ir {
+
+bool
+Program::makeFromTGSI(struct nv50_ir_prog_info *info,
+                      struct nv50_ir_prog_info_out *info_out)
+{
+   return false;
+}
+
+}
