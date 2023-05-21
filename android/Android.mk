6890d58d8dc8745e42b448d76e5551484f77d015
Mauro Rossi <issor.oruam@gmail.com>
2023-05-21 17:31:01 +0200

    AOSP: Add nouveau vulkan library suffix
    Required to correctly install vulkan.nouveau.so module

--- android/Android.mk
+++ android/Android.mk
@@ -36,6 +36,7 @@ MESA_VK_LIB_SUFFIX_broadcom := broadcom
 MESA_VK_LIB_SUFFIX_panfrost := panfrost
 MESA_VK_LIB_SUFFIX_virtio := virtio
 MESA_VK_LIB_SUFFIX_swrast := lvp
+MESA_VK_LIB_SUFFIX_nouveau-experimental := nouveau
 
 include $(CLEAR_VARS)
 
