70ffb033979475cea2d14e07a50d20549ad18f40
Mauro Rossi <issor.oruam@gmail.com>
2024-10-09 15:36:38 +0200

    nvk/android: Avoid building error in nak bindings
    Required to compile nvk for android
    
    Fixes the following building error:
    
    FAILED: src/nouveau/compiler/nak_bindings.rs
    ...
    /usr/include/xf86drm.h:40:10: fatal error: 'drm.h' file not found
    panicked at 'Unable to generate bindings: ClangDiagnostic("/usr/include/xf86drm.h:40:10:
    fatal error: 'drm.h' file not found\n")', main.rs:52:36
    
    Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>

--- src/nouveau/compiler/nak_bindings.h
+++ src/nouveau/compiler/nak_bindings.h
@@ -9,7 +9,9 @@
 #include "nouveau_context.h"
 #include "nouveau_device.h"
 
+#if !defined(ANDROID_STRICT)
 #include <xf86drm.h>
+#endif
 #include "drm-uapi/nouveau_drm.h"
 
 #define DRM_RS_IOCTL(FOO) \
