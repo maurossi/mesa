d5d2f68970432723a4128a84b9dbc6a06ba18b24
Mauro Rossi <issor.oruam@gmail.com>
2025-01-03 12:00:13 -0500

    HACK: Revert 27381_2024-03-08

--- src/vulkan/runtime/vk_instance.c
+++ src/vulkan/runtime/vk_instance.c
@@ -201,7 +201,7 @@ vk_instance_init(struct vk_instance *instance,
 
    instance->trace_mode = parse_debug_string(getenv("MESA_VK_TRACE"), trace_options);
    instance->trace_frame = (uint32_t)debug_get_num_option("MESA_VK_TRACE_FRAME", 0xFFFFFFFF);
-   instance->trace_trigger_file = secure_getenv("MESA_VK_TRACE_TRIGGER");
+   instance->trace_trigger_file = getenv("MESA_VK_TRACE_TRIGGER");
 
 #if !VK_LITE_RUNTIME_INSTANCE
    glsl_type_singleton_init_or_ref();
