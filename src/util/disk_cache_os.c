23f19f1a3c8ed808c8e495ebb1fc9e94f42df350
Mauro Rossi <issor.oruam@gmail.com>
2025-01-06 10:57:34 +0100

    HACK: Revert 27381_2024-11-28

--- src/util/disk_cache_os.c
+++ src/util/disk_cache_os.c
@@ -889,10 +889,10 @@ disk_cache_generate_cache_dir(void *mem_ctx, const char *gpu_name,
    else if (cache_type == DISK_CACHE_DATABASE)
       cache_dir_name = CACHE_DIR_NAME_DB;
 
-   char *path = secure_getenv("MESA_SHADER_CACHE_DIR");
+   char *path = getenv("MESA_SHADER_CACHE_DIR");
 
    if (!path) {
-      path = secure_getenv("MESA_GLSL_CACHE_DIR");
+      path = getenv("MESA_GLSL_CACHE_DIR");
       if (path)
          fprintf(stderr,
                  "*** MESA_GLSL_CACHE_DIR is deprecated; "
@@ -906,7 +906,7 @@ disk_cache_generate_cache_dir(void *mem_ctx, const char *gpu_name,
    }
 
    if (path == NULL) {
-      char *xdg_cache_home = secure_getenv("XDG_CACHE_HOME");
+      char *xdg_cache_home = getenv("XDG_CACHE_HOME");
 
       if (xdg_cache_home) {
          path = concatenate_and_mkdir(mem_ctx, xdg_cache_home, cache_dir_name);
