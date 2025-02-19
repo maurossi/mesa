b3ec7847a2084f3ab3cc655c998e612b77b924a3
Mauro Rossi <issor.oruam@gmail.com>
2025-02-19 15:37:21 -0500

    nvk/android: return result in nvk_image_layout()
    Error path is already doing it

--- src/nouveau/vulkan/nvk_image.c
+++ src/nouveau/vulkan/nvk_image.c
@@ -857,7 +857,7 @@ nvk_image_init_internal(struct nvk_device *dev,
    return VK_SUCCESS;
 }
 
-static void
+static VkResult
 nvk_image_layout(struct nvk_device *dev, struct nvk_image *image)
 {
    const struct nvk_physical_device *pdev = nvk_device_physical(dev);
@@ -1007,6 +1007,8 @@ nvk_image_layout(struct nvk_device *dev, struct nvk_image *image)
          return vk_errorf(dev, VK_ERROR_UNKNOWN,
                           "Invalid image creation parameters");
    }
+
+   return VK_SUCCESS;
 }
 
 static inline VkResult
@@ -1122,7 +1124,7 @@ nvk_image_init_cb(struct vk_device *device,
    if (result != VK_SUCCESS)
       return result;
 
-   nvk_image_layout(dev, image);
+   result = nvk_image_layout(dev, image);
 
    for (uint8_t plane = 0; plane < image->plane_count; plane++) {
       result = nvk_image_plane_alloc_va(dev, image, &image->planes[plane]);
@@ -1278,7 +1280,7 @@ nvk_GetDeviceImageMemoryRequirements(VkDevice device,
 
    result = nvk_image_init(dev, &image, pInfo->pCreateInfo);
    assert(result == VK_SUCCESS);
-   nvk_image_layout(dev, &image);
+   result = nvk_image_layout(dev, &image);
 
    const VkImageAspectFlags aspects =
       image.disjoint ? pInfo->planeAspect : image.vk.aspects;
@@ -1389,7 +1391,7 @@ nvk_GetDeviceImageSparseMemoryRequirements(
 
    result = nvk_image_init(dev, &image, pInfo->pCreateInfo);
    assert(result == VK_SUCCESS);
-   nvk_image_layout(dev, &image);
+   result = nvk_image_layout(dev, &image);
 
    const VkImageAspectFlags aspects =
       image.disjoint ? pInfo->planeAspect : image.vk.aspects;
@@ -1464,7 +1466,7 @@ nvk_GetDeviceImageSubresourceLayoutKHR(
 
    result = nvk_image_init(dev, &image, pInfo->pCreateInfo);
    assert(result == VK_SUCCESS);
-   nvk_image_layout(dev, &image);
+   result = nvk_image_layout(dev, &image);
 
    nvk_get_image_subresource_layout(dev, &image, pInfo->pSubresource, pLayout);
 
