82b81b14cec9e832da8b38c671efd80d0cd49ffe
Mauro Rossi <issor.oruam@gmail.com>
2025-02-19 15:37:21 -0500

    nvk/android: Add support for AHardwareBuffer
    AHB support for nvk based on 'AHB common' code in vulkan/runtime
    Create AHB memory is already implemented in vk_device_memory_create()
    
    Changelog:
    
    nvk_buffer.c: AHB support in nvk_GetPhysicalDeviceExternalBufferProperties()
    nvk_device_memory.c: AHB {buffer,image} memory properties and import AHB memory path in nvk_AllocateMamemory()
    nvk_image.c: AHB support in nvk_CreateImage(), nvk_GetPhysicalDeviceImageFormatProperties2()  and nvk_bind_image_memory()
    nvk_physical_device.c: Enable ANDROID_external_memory_android_hardware_buffer extension
    
    TODO:
    
    nvk_AllocateMemory(): 'else if (mem->vk.ahardware_buffer)' path for import of dma buf needs to be checked
    
    nvk_bind_image_memory(): image layout/modifier filling requires a new nvk_image_layout() function,
    with part of nvk_image_init() code, like in v3dv commit https://gitlab.freedesktop.org/mesa/mesa/-/commit/def20cbb07ac628a4b326d9bffcfeb3b88790b8e
    
    nvk_image_init(): image layout/modifier filling should be moved at the end, in order to bail out in case of AHB
    
    nvk_CreateImage(): should invoke nvk_image_init() and not bail out in case of AHB
    
    Achievements:
    
    PowerVR Vulkan Examples results
    AmbientOcclusion, GameOfLife, HelloAPI, MultiSampling, Subgroups: Working ok
    GaussianBlur: flickering artifacts on the right gaussian blurred texture
    
    dEQP-VK results not yet available
    
    (v2) based on Faith Ekstrand contributions nvk_image_layout(), nvk_image_alloc_vas()
    
    Changelog:
    
    nvk_device_memory.c: set AHB exportable image memory properties dinamically
    
    Achievements:
    
    PowerVR Vulkan Examples results
    AmbientOcclusion, GameOfLife, HelloAPI, MultiSampling, Subgroups: No regression observed
    GaussianBlur: Fixed
    
    (v3)

--- src/nouveau/vulkan/nvk_physical_device.c
+++ src/nouveau/vulkan/nvk_physical_device.c
@@ -269,6 +269,9 @@ nvk_get_device_extensions(const struct nvk_instance *instance,
       .EXT_ycbcr_image_arrays = true,
       .EXT_zero_initialize_device_memory = true,
 #if DETECT_OS_ANDROID
+#if ANDROID_API_LEVEL >= 26
+      .ANDROID_external_memory_android_hardware_buffer = vk_android_get_ugralloc() != NULL,
+#endif
       .ANDROID_native_buffer = vk_android_get_ugralloc() != NULL,
 #endif
       .GOOGLE_decorate_string = true,
