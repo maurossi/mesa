8b221df1601b958d8c93085378ecb31df1fd6e47
Mauro Rossi <issor.oruam@gmail.com>
2025-02-19 15:37:21 -0500

    nvk/android: Add support for AHardwareBuffer
    AHB support for nvk based on 'AHB common' code in vulkan/runtime
    Create AHB memory is already implemented in vk_device_memory_create()
    
    Changelog:
    
    nvk_buffer.c: AHB support in nvk_GetPhysicalDeviceExternalBufferProperties()
    nvk_device_memory.c: AHB {buffer,image} memory properties and import AHB memory path in nvk_AllocateMamemory()
    nvk_image.c: AHB support in nvk_CreateImage(), nvk_GetPhysicalDeviceImageFormatProperties2()  and nvk_bind_image_memory()
    nvk_physical_device.c: Enable ANDROID_external_memory_android_hardware_buffer extension
    
    TODO:
    
    nvk_AllocateMemory(): 'else if (mem->vk.ahardware_buffer)' path for import of dma buf needs to be checked
    
    nvk_bind_image_memory(): image layout/modifier filling requires a new nvk_image_layout() function,
    with part of nvk_image_init() code, like in v3dv commit https://gitlab.freedesktop.org/mesa/mesa/-/commit/def20cbb07ac628a4b326d9bffcfeb3b88790b8e
    
    nvk_image_init(): image layout/modifier filling should be moved at the end, in order to bail out in case of AHB
    
    nvk_CreateImage(): should invoke nvk_image_init() and not bail out in case of AHB
    
    Achievements:
    
    PowerVR Vulkan Examples results
    AmbientOcclusion, GameOfLife, HelloAPI, MultiSampling, Subgroups: Working ok
    GaussianBlur: flickering artifacts on the right gaussian blurred texture
    
    dEQP-VK results not yet available
    
    (v2) based on Faith Ekstrand contributions nvk_image_layout(), nvk_image_alloc_vas()
    
    Changelog:
    
    nvk_device_memory.c: set AHB exportable image memory properties dinamically
    
    Achievements:
    
    PowerVR Vulkan Examples results
    AmbientOcclusion, GameOfLife, HelloAPI, MultiSampling, Subgroups: No regression observed
    GaussianBlur: Fixed
    
    (v3) rebase on top of 73aa0f53 ("nvk: drop explicit u_gralloc init")
    
    Changelog:
    
    nvk_image.c: adaptations to nvk_image_layout() returning result
    nvk_physical_device.c: rebase on top of 73aa0f53 ("nvk: drop explicit u_gralloc init")
    
    (v4) rebase on 25.3.0 branch and minor change (use NVK_MAX_IMAGE_PLANES instead of 4)
    
    Changelog:
    
    nvk_image.c: use NVK_MAX_IMAGE_PLANES as number of planes in nvk_bind_image_memory()
    
    Vulkan Apps results report:
    
    vkCube:                          Not working (SIGABRT due to VK_ERROR_DEVICE_LOST )
    Abort message: 'cube.c:1096: void demo_draw(struct demo *): assertion "!err" failed'
    
    Sascha Willems Vulkan Examples:  Not working (SIGABRT due to VK_ERROR_DEVICE_LOST)
    Abort message: 'gears.cpp:328: void VulkanExample::draw(): assertion "res == VK_SUCCESS" failed'
    
    PowerVR Vulkan Examples that are working
    AmbientOcclusion, GameOfLife, GaussianBlur, HelloAPI, MultiSampling, Subgroups: No regression observed
    
    All the other PowerVR examples: Not working (SIGABRT due to VK_ERROR_DEVICE_LOST)
    Abort message: 'terminating due to uncaught exception of type pvrvk::ErrorDeviceLost: [VK_ERROR_DEVICE_LOST] : Failed to wait idle'
    
    [new tests] Khronos Vulkan-Samples that are working:
    api: hello_triangle, hpp_hello_triangle, swapchain_creation
    extensions: color_write_enable
    performance: image_compression_control, layout_transitions, pipeline_barriers, wait_idle
    
    All other Vulkan-Samples:       Not working (SIGABRT due to VK_ERROR_DEVICE_LOST)
    Abort message: 'terminating with uncaught exception of type std:runtime_error: Detected Vulkan error: ERROR_DEVICE_LOST'
    
    (v5) rebase on top of MR 36386
    
    No change in the code.
    
    The issues seen before were happening due to having used minigbm with dumb driver backend,
    which has only a minimalistic implementation.
    
    Using Glodroid's minigbm with gbm mesa backend,
    VkCube and PowerVR Vulkan Examples are working

--- src/nouveau/vulkan/nvk_device_memory.h
+++ src/nouveau/vulkan/nvk_device_memory.h
@@ -9,6 +9,7 @@
 
 #include "vk_device_memory.h"
 
+#include "util/detect_os.h"
 #include "util/list.h"
 
 struct nvkmd_mem;
@@ -24,5 +25,10 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(nvk_device_memory, vk.base, VkDeviceMemory,
 
 extern const VkExternalMemoryProperties nvk_opaque_fd_mem_props;
 extern const VkExternalMemoryProperties nvk_dma_buf_mem_props;
+#if DETECT_OS_ANDROID && ANDROID_API_LEVEL >= 26
+extern const VkExternalMemoryProperties nvk_ahb_buffer_mem_props;
+extern const VkExternalMemoryProperties nvk_ahb_image_mem_props;
+extern const VkExternalMemoryProperties nvk_ahb_image_mem_props_exportable;
+#endif
 
 #endif
