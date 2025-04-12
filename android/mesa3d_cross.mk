957181e00e9037d874939b5c962bdc80fd0cf9e3
Mauro Rossi <issor.oruam@gmail.com>
2025-04-12 03:36:31 -0400

    AOSP: add ~/.cargo/bin to PATH for bindgen specific version

--- android/mesa3d_cross.mk
+++ android/mesa3d_cross.mk
@@ -83,7 +83,7 @@ MESA3D_GLES_BINS := \
     $($(M_TARGET_PREFIX)MESA3D_LIBGLESV2_BIN) \
 
 MESON_GEN_NINJA := \
-	cd $(MESON_OUT_DIR) && PATH=/usr/bin:/usr/local/bin:$$PATH meson ./build     \
+	cd $(MESON_OUT_DIR) && PATH=~/.cargo/bin:/usr/bin:/usr/local/bin:$$PATH meson ./build     \
 	--cross-file $(call relative-to-absolute,$(MESON_GEN_DIR))/aosp_cross        \
 	--buildtype=release                                                          \
 	-Dplatforms=android                                                          \
