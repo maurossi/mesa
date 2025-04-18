d5c8a7ef3436ca94f79ab164870fe971990dbcc3
Mauro Rossi <issor.oruam@gmail.com>
2025-04-18 08:49:13 +0200

    AOSP: Add libzstd static dependency for Android 15 and later

--- android/Android.mk
+++ android/Android.mk
@@ -133,6 +133,7 @@ LOCAL_SHARED_LIBRARIES += \
     libhidlbase \
     libutils
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 35; echo $$?), 0)
+LOCAL_STATIC_LIBRARIES += libzstd
 LOCAL_SHARED_LIBRARIES += libui
 LOCAL_STATIC_LIBRARIES += libzstd
 MESON_GEN_PKGCONFIGS += ui
