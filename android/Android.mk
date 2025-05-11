d0bc23cb88bbf3fabeec4198e498734b3dc8679b
Mauro Rossi <issor.oruam@gmail.com>
2025-05-11 15:20:25 +0200

    android: fix llvm enabled drivers build
    llvmpipe driver name needs to be added to the list triggering MESON_GEN_LLVM_STUB := true
    
    swrast driver name is deprecared as gallium driver, but it is still used as vulkan driver

--- android/Android.mk
+++ android/Android.mk
@@ -57,7 +57,7 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 35; echo $$?), 0)
 LOCAL_STATIC_LIBRARIES += libc++fs
 endif
 
-ifneq ($(filter swrast,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
+ifneq ($(filter llvmpipe swrast,$(BOARD_MESA3D_GALLIUM_DRIVERS) $(BOARD_MESA3D_VULKAN_DRIVERS)),)
 ifeq ($(BOARD_MESA3D_FORCE_SOFTPIPE),)
 MESON_GEN_LLVM_STUB := true
 endif
